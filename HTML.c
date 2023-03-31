d13df9a5574c3c31e8a5877bdf27ed8c6e6a4d74
Emanuele Berdini <emanuele.berdo61@gmail.com>
2023-03-31 21:05:37 +0200

    aggiunta resoconto html

---
+++ HTML.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(char* x){
+if (x=="input"){
+    printf("%s","placeholder == mette nome dentro a text input /n type        == range(barra spostabile), checkbox(pallino), date, color 
+           password, text di default /n)");
+}
+}
