6716be45c2dacdcf0cce68fee2976e4f1ff51f9a
Emanuele Berdini <emanuele.berdo61@gmail.com>
2023-04-12 00:20:36 +0200

    a

--- link2_funct.js
+++ link2_funct.js
@@ -1,13 +1,12 @@
 function changecolor(){
     var col= document.getElementById("colore").value;
     var img=document.getElementById("jotaro");
-    img.style="border:solid"+col+"5px";
-    alert("ci va");
+    img.style="border:solid "+col+" 5px";
 }
 function changedim(){
     var range= document.getElementById("dimensione").value;
     var img=document.getElementById("jotaro");
     var x=(range*500)/100;
     img.height=x;
-    img.width=x*1.75;
+    img.width=x*1.78;
 }
